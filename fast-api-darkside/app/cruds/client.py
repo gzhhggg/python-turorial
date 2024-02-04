76fd59d0a0eccdbc508a3a2cd8da183ab835d063
gzhhggg <keiichimonoo@gmail.com>
2024-02-04 23:19:42 +0900

    complete client crud

--- fast-api-darkside/app/cruds/client.py
+++ fast-api-darkside/app/cruds/client.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import app.models.client as client_model
 import app.schemas.client as client_schema
 
@@ -6,3 +7,20 @@ async def create_client(client_data: client_schema.ClientCreate):
     client_obj = client_model.Client(name=client_data.name)
     await client_obj.save()
     return client_obj
+
+
+async def get_clients():
+    return await client_model.Client.filter(deleted_at=None)
+
+
+async def update_client(client_id: int, client_data: client_schema.ClientCreate):
+    client = await client_model.Client.get(id=client_id)
+    client.name = client_data.name
+    await client.save()
+    return client
+
+
+async def delete_client(client_id: int):
+    client = await client_model.Client.get(id=client_id)
+    client.deleted_at = datetime.now()
+    await client.save()
--- fast-api-darkside/app/routers/client.py
+++ fast-api-darkside/app/routers/client.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, HTTPException
+from typing import List
 import app.cruds.client as client_crud
 import app.schemas.client as client_schema
 
@@ -9,3 +10,21 @@ router = APIRouter()
 async def create_client(client: client_schema.ClientCreate):
     created_client = await client_crud.create_client(client)
     return created_client
+
+
+@router.get("/clients", response_model=List[client_schema.Client])
+async def get_clients():
+    clients = await client_crud.get_clients()
+    return clients
+
+
+@router.put("/clients/{client_id}", response_model=client_schema.Client)
+async def update_client(client_id: int, client: client_schema.ClientCreate):
+    updated_client = await client_crud.update_client(client_id, client)
+    return updated_client
+
+
+@router.delete("/clients/{client_id}", response_model=None, status_code=204)
+async def delete_client(client_id: int):
+    await client_crud.delete_client(client_id)
+    return {"message": "Client successfully deleted"}
